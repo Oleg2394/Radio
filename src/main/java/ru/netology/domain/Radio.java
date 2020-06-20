a819a9eda42b7ac7e1adb555212eb66d7824615d
Oleg <maczak94@yandex.ru>
2020-06-20 20:14:52 +0700

    add branch flexible

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,101 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation = 5;
+    private int maxStation = 10;
+    private int minStation = 0;
+    private int currentVolume = 5;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+
+    public Radio() {
+    }
+
+    public Radio(int currentStation, int maxStation, int currentVolume) {
+        this.currentStation = currentStation;
+        this.maxStation = maxStation;
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+     public void nextStation() {
+        if (currentStation == maxStation) {
+            currentStation = minStation;
+            return;
+        }
+        currentStation++;
+    }
+
+    public void prevStation() {
+        if (currentStation == minStation) {
+            currentStation = maxStation;
+            return;
+        }
+        currentStation--;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume += 1;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume -= 1;
+    }
+}
