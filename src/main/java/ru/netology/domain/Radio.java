fc31957f15bc6ef1706154c525b92b4dff20025c
Oleg2394 <50140221+Oleg2394@users.noreply.github.com>
2020-06-23 10:27:12 +0700

    Update Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,20 +1,14 @@
 package ru.netology.domain;
 
 public class Radio {
-    private int currentStation = 5;
+    private int currentStation;
     private int maxStation = 10;
     private int minStation = 0;
-    private int currentVolume = 5;
+    private int currentVolume;
     private int maxVolume = 100;
     private int minVolume = 0;
 
     public Radio() {
-    }
-
-    public Radio(int currentStation, int maxStation, int currentVolume) {
-        this.currentStation = currentStation;
-        this.maxStation = maxStation;
-        this.currentVolume = currentVolume;
     }
 
     public int getCurrentStation() {
@@ -89,13 +83,13 @@ public class Radio {
         if (currentVolume == maxVolume) {
             return;
         }
-        currentVolume += 1;
+        currentVolume ++;
     }
 
     public void decreaseVolume() {
         if (currentVolume == minVolume) {
             return;
         }
-        currentVolume -= 1;
+        currentVolume --;
     }
 }
